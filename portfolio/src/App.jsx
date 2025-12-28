98e96d44331e27ab9a169c2a1d384261585e3b7e
Shreya Bhardwaj <shreyabhardwaj354@gmail.com>
2025-12-28 23:09:44 +0530

    day1

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar.jsx";
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Navbar.jsx
@@ -0,0 +1,54 @@
+export default function Navbar() {
+  return (
+    <div className="h-20  flex justify-between items-center p-6 w-full">
+      <div className="flex w-[35%] text-3xl font-bold ">S</div>
+      <nav
+        className="hidden items-center text-gray-500 md:flex md:gap-8
+      p-12"
+      >
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-bl transition-color duration-300 "
+        >
+          Home
+        </a>
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-fuchsia-900 transition-color duration-300 "
+        >
+          About
+        </a>
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-fuchsia-900 transition-color duration-300 "
+        >
+          Experience
+        </a>
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-fuchsia-900 transition-color duration-300 "
+        >
+          Skills
+        </a>
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-fuchsia-900 transition-color duration-300 "
+        >
+          Projects
+        </a>
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-fuchsia-900 transition-color duration-300 "
+        >
+          Services
+        </a>
+        <a
+          href=""
+          className="hover:text-white hover:border-b-1  hover:border-b-fuchsia-900 transition-color duration-300 "
+        >
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
+}
